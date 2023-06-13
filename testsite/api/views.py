d5c622dff96e389706a0d1c905e8a47da184facc
tristanpank <tristanpank@gmail.com>
2023-06-13 16:53:06 -0600

    Installed/used MoviePy for video conversion

--- testsite/api/views.py
+++ testsite/api/views.py
@@ -10,6 +10,7 @@ from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import viewsets
 import subprocess
 from django.conf import settings
+import moviepy.editor as moviepy
 
 class BlogList(generics.ListCreateAPIView):
     
@@ -68,13 +69,18 @@ class FilePostViewSet(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         new_file = serializer.save(owner=self.request.user)
         if new_file.title[-3:] == "mov":
-            image_path = str(settings.BASE_DIR)[:-9] + '\\media\\images\\'
+            image_path = str(settings.BASE_DIR)[:-9] + '\\media\\'
             mp4_name = new_file.title[:-3] + "mp4"
-            ffmpeg_command = 'ffmpeg i ' + image_path + str(new_file.title) + ' ' + image_path + mp4_name
-            print(ffmpeg_command)
-            subprocess.call('ffmpeg i ' + image_path + str(new_file.title) + ' ' + image_path + mp4_name)
+            mp4_url = str(new_file.file_url)[:-3] + "mp4"
+            # print(mp4_url)
+            clip = moviepy.VideoFileClip(image_path + str(new_file.file_url))
+            clip.write_videofile(image_path + mp4_url)
+
+            # ffmpeg_command = 'ffmpeg i ' + image_path + str(new_file.title) + ' ' + image_path + mp4_name
+            # print(ffmpeg_command)
+            # subprocess.call('ffmpeg i ' + image_path + str(new_file.title) + ' ' + image_path + mp4_name)
             new_file.title = mp4_name
-            new_file.file_url = str(new_file.file_url)[:-3] + "mp4"
+            new_file.file_url = mp4_url
             new_file.save()
 
         
